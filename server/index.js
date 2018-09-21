aed1ca4dd28efffd0c1b8c9e43e4df57b4068a41
pbgnz <pabgonza1995@gmail.com>
2018-09-21 16:19:14 -0400

    setup api

--- server/index.js
+++ server/index.js
@@ -32,3 +32,38 @@ const redisClient = redis.createClient({
     retry_strategy: () => 1000
 });
 const redistPublisher = redisClient.duplicate();
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Calculadora');
+});
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * from values');
+    res.send(values.rows);
+})
+
+app.get('/values/current', async (req, res) => {
+    redisClient.hgetall('values', (err, values) => {
+        res.send(values);
+    });
+    res.send(values.rows);
+})
+
+app.post('/values', async (req, res) => {
+    const index = req.body.index;
+    
+    if (parseInt(index) > 40) {
+        return res.status(422).send('Index too high');
+    }
+
+    redisClient.hset('values', index, 'N/A');
+    redistPublisher.publish('insert', index);
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+    res.send({working: true});
+});
+
+app.listen(5000, err => {
+    console.log('Node server listening on port 5000');
+})
